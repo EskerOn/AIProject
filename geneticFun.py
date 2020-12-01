86067b702926af2eb566340f8287fde0ff392fbc
EskerOn <35613070+EskerOn@users.noreply.github.com>
2020-12-01 01:47:18 -0600

    Update geneticFun.py

--- geneticFun.py
+++ geneticFun.py
@@ -1,12 +1,4 @@
 import random
-
-MAXDEC = 3 	# Cantidad de decimales en los float
-MAXEP = 3000  	# Máximo de etapas
-MINEP = 50		# Mínimo de etapas
-NPARA = 13		# Cantidad de parámetros
-NCLAS = 4		# Cantidad de clases
-MAXCAP = 3      # Máximo de capas
-SD = []
 #Formato de genético
 #list[x, y, z, v, w] : los rangos están de 0 a N y todos son int
 # x neuronas x capa
@@ -27,136 +19,148 @@ SD = []
 |   etapas                  |       x
 """
 
-def initSD(): #inicializa los rangos de valores y bits de los atributos
-    SD.append([NPARA, bitsTo(NPARA)])
-    SD.append([MAXCAP,bitsTo(MAXCAP)])
-    SD.append([pow(10, MAXDEC), bitsTo(pow(10, MAXDEC))])
-    SD.append([pow(10, MAXDEC), bitsTo(pow(10, MAXDEC))])
-    SD.append([MAXEP-MINEP,bitsTo(MAXEP-MINEP)])
-
-def bitsTo(aux): #Dice cuántos bits son necesarios para un rango
-    s = 1
-    while(aux // 2 > 0):
-        aux = aux//2
-        s += 1
-    return s
-
-
-def translate(data : list):#Traduce el formato genético a ANN
-	return [data[2]/pow(10, MAXDEC), data[3]/pow(10, MAXDEC), data[4] + MINEP, [data[0] + NCLAS for i in range(data[1] + 1)]] 
-
-
-def randIndividual(): #genera el formato genético, línea 10
-    aux = []
-    for i in range(5):
-        aux.append(random.randrange(SD[i][0]+1))
-    return aux
-
-def tobin(data : list):#Muestra la forma binaria del formato genético
-    out = ""
-    for i in range(5):
-        out += tobins(data[i], SD[i][1])
-    return(out)
-
-def toformgen(genes : int):
-    out = []
-    for i in range(5):
-        mask = (1 << SD[4-i][1]) - 1
-#        out.append(genes & mask)
-        out.insert(0, genes & mask)
-        genes = genes >> SD[4-i][1]
-    return out
-
-
-def tobins(a : int, size : int): #Muestra la forma binaria de un número
-    out = bin(a)[2:]
-    while(len(out) < size):
-            out = "0"+out
-    return out
-
-def mutation(data : list):#Hace una mutación en un parámetro aleatorio del individuo, trabaja sobre el formato genético
-    out = [data[i] for i in range(5)]
-    aux = random.randrange(5)
-    print("voy a mutar el parámetro: ", aux)
-    maskaux = pow(2, SD[aux][1]) - 1
-    mask = pow(2, random.randrange(SD[aux][1]))  
-    if mask & data[aux] > 0:#bit encendido
-        out[aux] = ((data[aux] ^ maskaux) | mask) ^ maskaux
-    else:
-        out[aux] = data[aux] | mask
-    while(out[aux] > SD[aux][0]):
-        mask = pow(2, random.randrange(SD[aux][1]))  
-        if mask & data[aux] > 0:#bit encendido
-            out[aux] = ((data[aux] ^ maskaux) | mask) ^ maskaux
-        else:
-            out[aux] = data[aux] | mask
-    return out
-
-def cruza(padre : list, madre : list):#Recibe dos formatos genéticos y devuelve uno resultado de los dos
-    padres = tobin(padre)
-    madres = tobin(madre)
-    punto = random.randrange(3, len(padres) - 3)
-    print("punto de cruza: ", punto)
-    mask1 = (1 << punto) - 1
-    mask2 = ((1 << (len(padres)-punto)) - 1) << punto
-    h1 = (int(padres, 2) & mask1) | (int(madres, 2) & mask2)
-    h2 = (int(padres, 2) & mask2) | (int(madres, 2) & mask1)
-    return toformgen(h1), toformgen(h2)
-        
-
-
-def fitness(data : list): #trabaja sobre el formato genético
+def fitness(self, data : list): #trabaja sobre el formato genético
     #aquí, de alguna manera se hace el llamado a WEKA
     #así que
     #return WEKA(translate(data))
     return random.randint(0, 100)
 
-"""def GA(Fitness_threshold : float, P : int, R : float, M : int):
-    population=[]
-    populationS=[]
-    for i in range(P):
-        population.append(randIndividual())
-    for element in population:
-        element.FitnessValue=fitness(element)
-    while():
-        sample=random.sample(population, int((1-R)*P))
-        for item in sample:
-            if(): ###aqui va la probabilidad Pr(hi)
-                populationS.append(item)
-        pairs=[(population[i],population[j]) for i in range(len(population)) for j in range(i+1, len(population))]
-        sample=random.sample(pairs, int(R*P/2))
-        for item in sample:
-            if(): ###aqui va la probabilidad Pr(hi)
-                pass ##aqui se hace el crosover
-                ##se añaden a PopulationS
-        sample=random.sample(populationS, int(len(populationS)/100*M))
-        for item in sample:
-            pass ##se aplica mutación sobre item
-        population=populationS
+class GeneticA():
+    def __init__(self, MAXDEC, MAXEP, MINEP, NATRIB, NCLAS, MAXCAP, FITNESS):
+        self.settings=[MAXDEC, MAXEP, MINEP, NATRIB, NCLAS, MAXCAP]
+        self.SD=[]
+        self.SD.append([NATRIB, self.bitsTo(NATRIB)])
+        self.SD.append([MAXCAP-1,self.bitsTo(MAXCAP-1)])
+        self.SD.append([pow(10, MAXDEC)-50, self.bitsTo(pow(10, MAXDEC)-50)])
+        self.SD.append([pow(10, MAXDEC)-50, self.bitsTo(pow(10, MAXDEC)-50)])
+        self.SD.append([MAXEP-MINEP, self.bitsTo(MAXEP-MINEP)])
+        #GA()
+    def bitsTo(self, aux): #Dice cuántos bits son necesarios para un rango
+        s = 1
+        while(aux // 2 > 0):
+            aux = aux//2
+            s += 1
+        return s
+
+    def translate(self, data : list):#Traduce el formato genético a ANN
+        return [data[2]/pow(10, self.settings[0]), data[3]/pow(10, self.settings[0]), data[4] + self.settings[2], [data[0] + self.settings[5] for i in range(data[1] + 1)]]
+
+    def randIndividual(self): #genera el formato genético, línea 10
+        aux = []
+        for i in range(5): 
+            aux.append(random.randrange(self.SD[i][0]+1))
+        return aux
+
+    def tobin(self, data : list):#Muestra la forma binaria del formato genético
+        out = ""
+        for i in range(5):
+            out += self.tobins(data[i], self.SD[i][1])
+        return(out)
+
+    def tobins(self, a : int, size : int): #Muestra la forma binaria de un número
+        out = bin(a)[2:]
+        while(len(out) < size):
+                out = "0"+out
+        return out
+
+    def toformgen(self, genes : int):
+        out = []
+        for i in range(5):
+            mask = (1 << self.SD[4-i][1]) - 1
+    #        out.append(genes & mask)
+            out.insert(0, genes & mask)
+            genes = genes >> self.SD[4-i][1]
+        for i in range(5):
+            if out[i]>self.SD[i][0]:
+                out[i]=self.SD[i][0]
+        return out
+
+
+    def mutation(self, data : list):#Hace una mutación en un parámetro aleatorio del individuo, trabaja sobre el formato genético
+        out = [i for i in data]
+        aux = random.randrange(5)
+        print("voy a mutar el parámetro: ", aux)
+        maskaux = pow(2, self.SD[aux][1]) - 1
+        mask = pow(2, random.randrange(self.SD[aux][1]))  
+        if mask & data[aux] > 0:#bit encendido
+            out[aux] = ((data[aux] ^ maskaux) | mask) ^ maskaux
+        else:
+            out[aux] = data[aux] | mask
+        while(out[aux] > self.SD[aux][0]):
+            mask = pow(2, random.randrange(self.SD[aux][1]))  
+            if mask & data[aux] > 0:#bit encendido
+                out[aux] = ((data[aux] ^ maskaux) | mask) ^ maskaux
+            else:
+                out[aux] = data[aux] | mask
+        return out
+
+    def cruza(self, padre : list, madre : list):#Recibe dos formatos genéticos y devuelve uno resultado de los dos
+        padres = self.tobin(padre)
+        madres = self.tobin(madre)
+        punto = random.randrange(3, len(padres) - 3)
+        print("punto de cruza: ", punto)
+        mask1 = (1 << punto) - 1
+        mask2 = ((1 << (len(padres)-punto)) - 1) << punto
+        h1 = (int(padres, 2) & mask1) | (int(madres, 2) & mask2)
+        h2 = (int(padres, 2) & mask2) | (int(madres, 2) & mask1)
+        return self.toformgen(h1), self.toformgen(h2)
+        
+
+    """def GA(self, Fitness_threshold : float, P : int, R : float, M : int):
+        population=[]
+        populationS=[]
+        for i in range(P):
+            population.append(randIndividual())
         for element in population:
             element.FitnessValue=fitness(element)
-    return max(population)"""
+        while():
+            sample=random.sample(population, int((1-R)*P))
+            for item in sample:
+                if(): ###aqui va la probabilidad Pr(hi)
+                    populationS.append(item)
+            pairs=[(population[i],population[j]) for i in range(len(population)) for j in range(i+1, len(population))]
+            sample=random.sample(pairs, int(R*P/2))
+            for item in sample:
+                if(): ###aqui va la probabilidad Pr(hi)
+                    pass ##aqui se hace el crosover
+                    ##se añaden a PopulationS
+            sample=random.sample(populationS, int(len(populationS)/100*M))
+            for item in sample:
+                pass ##se aplica mutación sobre item
+            population=populationS
+            for element in population:
+                element.FitnessValue=fitness(element)
+        return max(population)"""
+
+MAXDEC = 2 	# Cantidad de decimales en los float
+MAXEP = 500  	# Máximo de etapas
+MINEP = 50		# Mínimo de etapas
+NATRIB = 13		# Cantidad de parámetros
+NCLAS = 4		# Cantidad de clases
+MAXCAP = 3      # Máximo de capas
 
 
-initSD()
-print(SD)
+gen=GeneticA(MAXDEC, MAXEP, MINEP, NATRIB, NCLAS, MAXCAP, fitness)
+#print(SD)
 #ejemplos
-aux = randIndividual()
+aux = gen.randIndividual()
 print("dos individuos aleatorios, ya cumplen con el rango")
 print(aux)
-aux2 = randIndividual()
+aux2 = gen.randIndividual()
 print(aux2)
 print("su representación binaria")
-print(tobin(aux))
-print(tobin(aux2))
+print(gen.tobin(aux))
+print(gen.tobin(aux2))
 print("los resultados de la cruza, no necesariamente cimplen el rango")
-h1, h2 = cruza(aux, aux2)
+h1, h2 = gen.cruza(aux, aux2)
 print(h1)
 print(h2)
 print("su representación binaria")
-print((tobin(h1)))
-print((tobin(h2)))
-m1 = mutation(h1)
+print((gen.tobin(h1)))
+print((gen.tobin(h2)))
+m1 = gen.mutation(h1)
 print("mutacion del hijo 1")
 print(m1)
-print(tobin(m1))
+print(gen.tobin(m1))
+print(gen.translate(aux))
+print(gen.translate(h1))
