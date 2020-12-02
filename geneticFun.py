3a3284c228c0e2754de3dcc4342cb000e1e81440
EskerOn <35613070+EskerOn@users.noreply.github.com>
2020-12-01 22:48:43 -0600

    Update geneticFun.py

--- geneticFun.py
+++ geneticFun.py
@@ -9,6 +9,7 @@ class Individual:
         self.HiddenLayers = HiddenLayers
         self.FitnessValue = None
         self.GeneticForm = None
+        self.probability = 0
     def settings(self):
         Hlayers = ','.join(map(str, self.HiddenLayers)) 
         return ["-L",str(self.LearningR), "-M", str(self.Momentum), "-N", str(self.Period), "-V", "0", "-S", "0", "-E", "20", "-H", Hlayers]
@@ -37,7 +38,7 @@ class Individual:
 """
 
 def fitness(toeval : Individual): 
-    return random.randint(0, 100)
+    return toeval.Period*random.randint(0,50)/1000*3.5
 
 class GeneticA():
     def __init__(self, MAXDEC, MAXEP, MINEP, NATRIB, NCLAS, MAXCAP, FITNESS):
@@ -48,6 +49,9 @@ class GeneticA():
         self.SD.append([pow(10, MAXDEC)-50, self.bitsTo(pow(10, MAXDEC)-50)])
         self.SD.append([pow(10, MAXDEC)-50, self.bitsTo(pow(10, MAXDEC)-50)])
         self.SD.append([MAXEP-MINEP, self.bitsTo(MAXEP-MINEP)])
+        self.sumFitness=0
+        self.maxFit=Individual(0,0,0,[])
+        self.maxFit.FitnessValue=0
         #GA()
     def bitsTo(self, aux): #Dice cuántos bits son necesarios para un rango
         s = 1
@@ -88,7 +92,7 @@ class GeneticA():
             genes = genes >> self.SD[4-i][1]
         for i in range(5):
             if out[i]>self.SD[i][0]:
-                print("------------------------fuera de rango {} ".format(out[i]))
+                #print("------------------------fuera de rango {} ".format(out[i]))
                 out[i]=self.SD[i][0]
         ind=Individual(out[2]/pow(10, self.settings[0]), out[3]/pow(10, self.settings[0]), out[4] + self.settings[2], [out[0] + self.settings[5] for i in range(out[1] + 1)])
         ind.GeneticForm=out
@@ -97,7 +101,7 @@ class GeneticA():
     def mutation(self, data : list):#Hace una mutación en un parámetro aleatorio del individuo, trabaja sobre el formato genético
         out = [i for i in data]
         aux = random.randrange(5)
-        print("voy a mutar el parámetro: ", aux)
+        #print("voy a mutar el parámetro: ", aux)
         maskaux = pow(2, self.SD[aux][1]) - 1
         mask = pow(2, random.randrange(self.SD[aux][1]))  
         if mask & data[aux] > 0:#bit encendido
@@ -118,23 +122,25 @@ class GeneticA():
         padres = self.tobin(padre)
         madres = self.tobin(madre)
         punto = random.randrange(3, len(padres) - 3)
-        print("punto de cruza: ", punto)
+        #print("punto de cruza: ", punto)
         mask1 = (1 << punto) - 1
         mask2 = ((1 << (len(padres)-punto)) - 1) << punto
         h1 = (int(padres, 2) & mask1) | (int(madres, 2) & mask2)
         h2 = (int(padres, 2) & mask2) | (int(madres, 2) & mask1)
         return self.toformgen(h1), self.toformgen(h2)
 
-    def pr(self, val):
-        return random.randint(0, 100)
-
+    """def pr(self, element):
+        val=random.randint(0, self.sumFitness)
+        if(random.randint(0, element.FitnessValue);)      
+"""
     def maxim(self, population):
         maxitem=population[0]
         for item in population:
             if item.FitnessValue>maxitem.FitnessValue:
                 maxitem=item
         return maxitem
-
+    def takefit(self, elem):
+        return elem.FitnessValue
     def GA(self, Fitness_threshold : float, P : int, R : float, M : int, fitness):
         population=[]
         populationS=[]
@@ -142,36 +148,58 @@ class GeneticA():
             population.append(self.randIndividual())
         for element in population:
             element.FitnessValue=fitness(element)
-        iteraciones=0
-        while(iteraciones<5000):
-            sample=random.sample(population, int((1-R)*P))
+        self.sumFitness= sum([i.FitnessValue for i in population])
+        #for element in population:
+        #    element.probability=element.FitnessValue//self.sumFitness
+        population.sort(key=self.takefit, reverse=True)
+        gen=0
+        while(self.maxFit.FitnessValue<Fitness_threshold):
+            print("Generation: " + str(gen) + " Fittest: " + str(self.maxFit.FitnessValue))
+            #print("population: ", end="")
+            #toprint=[element.FitnessValue for element in population]
+            #print(toprint)
+
+            sample=population[:int((1-R)*P)]
+            #print("sample: ", end="")
+            #toprint=[element.FitnessValue for element in sample]
+            #print(toprint)
+            #sample=random.sample(population, int((1-R)*P))
             for item in sample:
-                if(self.pr(item)): ###aqui va la probabilidad Pr(hi)
-                    populationS.append(item)
-            pairs=[(population[i],population[j]) for i in range(len(population)) for j in range(i+1, len(population))]
-            sample=random.sample(pairs, int(R*P/2))
+                #if(self.pr(item)): ###aqui va la probabilidad Pr(hi)
+                populationS.append(item)
+            topair=population[:int(R*P)]
+            pairs=[(topair[i],topair[i+1]) for i in range(len(topair)-1)]
+            #sample=random.sample(pairs, int(R*P/2))
+            for item in pairs:
+                #if(self.pr(item)): ###aqui va la probabilidad Pr(hi)
+                h1, h2 = self.cruza(item[0].GeneticForm, item[1].GeneticForm)
+                populationS.append(h1)
+                populationS.append(h2)
+            sample=[]
+            for i in range(int(len(populationS)*M)):
+                sample.append(populationS.pop(random.randrange(len(populationS))))
             for item in sample:
-                if(self.pr(item)): ###aqui va la probabilidad Pr(hi)
-                    h1, h2 = self.cruza(item[0].GeneticForm, item[1].GeneticForm)
-                    populationS.append(h1)
-                    populationS.append(h2)
-            sample=random.sample(populationS, int(len(populationS)/100*M))
-            for item in sample:
-                item = self.mutation(item.GeneticForm) ##se aplica mutación sobre item
+                mutated = self.mutation(item.GeneticForm) ##se aplica mutación sobre item
+                populationS.append(mutated)
             population=populationS
             for element in population:
                 element.FitnessValue=fitness(element)
-            iteraciones+=1
-        return self.maxim(population)
+            population.sort(key=self.takefit, reverse=True)
+            self.maxFit=self.maxim(population)
+            gen+=1
+        return self.maxFit
 
 MAXDEC = 2 	# Cantidad de decimales en los float
-MAXEP = 500  	# Máximo de etapas
+MAXEP = 577  	# Máximo de etapas
 MINEP = 50		# Mínimo de etapas
 NATRIB = 13		# Cantidad de parámetros
 NCLAS = 4		# Cantidad de clases
 MAXCAP = 3      # Máximo de capas
 
-
+gen=GeneticA(MAXDEC, MAXEP, MINEP, NATRIB, NCLAS, MAXCAP, fitness)
+val=gen.GA(100, 10, 0.4, 0.2, fitness)
+print("finalizó max: " + str(val.FitnessValue))
+"""
 gen=GeneticA(MAXDEC, MAXEP, MINEP, NATRIB, NCLAS, MAXCAP, fitness)
 #print(SD)
 #ejemplos
@@ -194,3 +222,4 @@ m1 = gen.mutation(h1.GeneticForm)
 print("mutacion del hijo 1")
 print(m1)
 print(gen.tobin(m1.GeneticForm))
+"""
